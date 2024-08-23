85e406ae9af39b601db44f65a68a25b91600f33f
Victor Herrero Otal <victor.herrero.otal@sap.com>
2024-08-23 17:20:35 +0200

    Add alert for capped VPA recommendations for the seed
    This alert needs to be deployed within the aggregate Prometheus because
    this one is connected with the seed Alertmanager. Also, the garden
    Prometheus federates from it so this is a way to have these alerts also
    available in the garden Prometheus.

--- pkg/component/observability/monitoring/prometheus/aggregate/prometheusrules_test.go
+++ pkg/component/observability/monitoring/prometheus/aggregate/prometheusrules_test.go
@@ -19,8 +19,53 @@ import (
 
 var _ = Describe("PrometheusRules", func() {
 	Describe("#CentralPrometheusRules", func() {
-		It("should return the expected objects", func() {
-			Expect(aggregate.CentralPrometheusRules()).To(HaveExactElements(
+		rules := []monitoringv1.Rule{
+			{
+				Alert: "PodStuckInPending",
+				Expr:  intstr.FromString(`sum_over_time(kube_pod_status_phase{phase="Pending"}[5m]) > 0`),
+				For:   ptr.To(monitoringv1.Duration("10m")),
+				Labels: map[string]string{
+					"severity":   "warning",
+					"type":       "seed",
+					"visibility": "operator",
+				},
+				Annotations: map[string]string{
+					"description": "Pod {{$labels.pod}} in namespace {{$labels.namespace}} was stuck in Pending state for more than 10 minutes.",
+					"summary":     "A pod is stuck in pending",
+				},
+			},
+			{
+				Alert: "NodeNotHealthy",
+				Expr:  intstr.FromString(`count_over_time((sum by (node) (kube_node_spec_taint{effect="NoSchedule", key!~"node.kubernetes.io/unschedulable|deployment.machine.sapcloud.io/prefer-no-schedule|ToBeDeletedByClusterAutoscaler|` + v1beta1constants.TaintNodeCriticalComponentsNotReady + `"}))[30m:]) > 9`),
+				For:   ptr.To(monitoringv1.Duration("0m")),
+				Labels: map[string]string{
+					"severity":   "warning",
+					"type":       "seed",
+					"visibility": "operator",
+				},
+				Annotations: map[string]string{
+					"description": "Node {{$labels.node}} in seed {{$externalLabels.seed}} was not healthy for ten scrapes in the past 30 mins.",
+					"summary":     "A node is not healthy.",
+				},
+			},
+			{
+				Alert: "TooManyEtcdSnapshotCompactionJobsFailing",
+				Expr:  intstr.FromString(`count(increase(etcddruid_compaction_jobs_total{succeeded="false"}[3h]) >= 1) / count(increase(etcddruid_compaction_jobs_total[3h])) > 0.1`),
+				Labels: map[string]string{
+					"severity":   "warning",
+					"type":       "seed",
+					"visibility": "operator",
+				},
+				Annotations: map[string]string{
+					"description": "Seed {{$externalLabels.seed}} has too many etcd snapshot compaction jobs failing in the past 3 hours.",
+					"summary":     "Too many etcd snapshot compaction jobs are failing in the seed.",
+				},
+			},
+		}
+
+		It("should return the expected objects when seed is also the garden cluster", func() {
+			seedIsGarden := true
+			Expect(aggregate.CentralPrometheusRules(seedIsGarden)).To(HaveExactElements(
 				PointTo(MatchFields(IgnoreExtras, Fields{
 					"TypeMeta":   MatchFields(IgnoreExtras, Fields{"APIVersion": Equal("monitoring.coreos.com/v1"), "Kind": Equal("PrometheusRule")}),
 					"ObjectMeta": MatchFields(IgnoreExtras, Fields{"Name": Equal("metering-stateful")}),
@@ -29,50 +74,47 @@ var _ = Describe("PrometheusRules", func() {
 					ObjectMeta: metav1.ObjectMeta{Name: "seed"},
 					Spec: monitoringv1.PrometheusRuleSpec{
 						Groups: []monitoringv1.RuleGroup{{
-							Name: "seed.rules",
-							Rules: []monitoringv1.Rule{
-								{
-									Alert: "PodStuckInPending",
-									Expr:  intstr.FromString(`sum_over_time(kube_pod_status_phase{phase="Pending"}[5m]) > 0`),
-									For:   ptr.To(monitoringv1.Duration("10m")),
-									Labels: map[string]string{
-										"severity":   "warning",
-										"type":       "seed",
-										"visibility": "operator",
-									},
-									Annotations: map[string]string{
-										"description": "Pod {{$labels.pod}} in namespace {{$labels.namespace}} was stuck in Pending state for more than 10 minutes.",
-										"summary":     "A pod is stuck in pending",
-									},
-								},
-								{
-									Alert: "NodeNotHealthy",
-									Expr:  intstr.FromString(`count_over_time((sum by (node) (kube_node_spec_taint{effect="NoSchedule", key!~"node.kubernetes.io/unschedulable|deployment.machine.sapcloud.io/prefer-no-schedule|ToBeDeletedByClusterAutoscaler|` + v1beta1constants.TaintNodeCriticalComponentsNotReady + `"}))[30m:]) > 9`),
-									For:   ptr.To(monitoringv1.Duration("0m")),
-									Labels: map[string]string{
-										"severity":   "warning",
-										"type":       "seed",
-										"visibility": "operator",
-									},
-									Annotations: map[string]string{
-										"description": "Node {{$labels.node}} in seed {{$externalLabels.seed}} was not healthy for ten scrapes in the past 30 mins.",
-										"summary":     "A node is not healthy.",
-									},
-								},
-								{
-									Alert: "TooManyEtcdSnapshotCompactionJobsFailing",
-									Expr:  intstr.FromString(`count(increase(etcddruid_compaction_jobs_total{succeeded="false"}[3h]) >= 1) / count(increase(etcddruid_compaction_jobs_total[3h])) > 0.1`),
-									Labels: map[string]string{
-										"severity":   "warning",
-										"type":       "seed",
-										"visibility": "operator",
-									},
-									Annotations: map[string]string{
-										"description": "Seed {{$externalLabels.seed}} has too many etcd snapshot compaction jobs failing in the past 3 hours.",
-										"summary":     "Too many etcd snapshot compaction jobs are failing in the seed.",
-									},
-								},
-							},
+							Name:  "seed.rules",
+							Rules: rules,
+						}},
+					},
+				}),
+			))
+		})
+
+		It("should return the expected objects when seed and the garden clusters are different", func() {
+			seedIsGarden := false
+			vpaRule := monitoringv1.Rule{
+				Alert: "VerticalPodAutoscalerCappedRecommendation",
+				Expr: intstr.FromString(`
+    ( {__name__=~"kube_customresource_verticalpodautoscaler_status_recommendation_containerrecommendations_uncappedtarget_.+"} > 0 )
+>
+    ( {__name__=~"kube_customresource_verticalpodautoscaler_status_recommendation_containerrecommendations_target_.+"} > 0 )`),
+				Labels: map[string]string{
+					"severity":   "warning",
+					"type":       "seed",
+					"visibility": "operator",
+				},
+				Annotations: map[string]string{
+					"summary": "A VPA recommendation is capped.",
+					"description": "The {{ $labels.verticalpodautoscaler }} VPA " +
+						"in the {{ $labels.namespace }} namespace " +
+						"in the {{ $externalLabels.seed }} seed has a " +
+						"{{ if eq .Labels.unit \"core\" -}} CPU {{- else if eq .Labels.unit \"byte\" -}} memory {{- end }}" +
+						"uncapped target recommendation larger than the regular target recommendation for the {{ $labels.container }} container.",
+				},
+			}
+			Expect(aggregate.CentralPrometheusRules(seedIsGarden)).To(HaveExactElements(
+				PointTo(MatchFields(IgnoreExtras, Fields{
+					"TypeMeta":   MatchFields(IgnoreExtras, Fields{"APIVersion": Equal("monitoring.coreos.com/v1"), "Kind": Equal("PrometheusRule")}),
+					"ObjectMeta": MatchFields(IgnoreExtras, Fields{"Name": Equal("metering-stateful")}),
+				})),
+				Equal(&monitoringv1.PrometheusRule{
+					ObjectMeta: metav1.ObjectMeta{Name: "seed"},
+					Spec: monitoringv1.PrometheusRuleSpec{
+						Groups: []monitoringv1.RuleGroup{{
+							Name:  "seed.rules",
+							Rules: append(rules, vpaRule),
 						}},
 					},
 				}),
