65ad60d958bc2336b2e860673a15a1dc070c2172
Victor Herrero Otal <victor.herrero.otal@sap.com>
2025-02-12 13:41:48 +0100

    Introduce go pkg for the metering Prometheus

---
+++ pkg/component/observability/monitoring/prometheus/metering/component.go
@@ -0,0 +1,8 @@
+// SPDX-FileCopyrightText: SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package metering
+
+// Label is a constant for the label of the metering prometheus instance.
+const Label = "metering"
