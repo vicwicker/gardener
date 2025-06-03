a9e6aad48a6f9bb9fc9b2d2819476c51dbbd1616
Victor Herrero Otal <victor.herrero.otal@sap.com>
2025-06-03 13:35:40 +0200

    With Pod

--- cmd/gardenlet/app/migration.go
+++ cmd/gardenlet/app/migration.go
@@ -8,16 +8,22 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"time"
 
 	"github.com/go-logr/logr"
 	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	gardencorev1beta1 "github.com/gardener/gardener/pkg/apis/core/v1beta1"
 	v1beta1helper "github.com/gardener/gardener/pkg/apis/core/v1beta1/helper"
+	extensionsv1alpha1 "github.com/gardener/gardener/pkg/apis/extensions/v1alpha1"
 	"github.com/gardener/gardener/pkg/features"
 	"github.com/gardener/gardener/pkg/utils/flow"
 	gardenerutils "github.com/gardener/gardener/pkg/utils/gardener"
+	"github.com/gardener/gardener/pkg/utils/retry"
+	monitoringv1 "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
 )
 
 func (g *garden) runMigrations(ctx context.Context, log logr.Logger, gardenClient client.Client) error {
@@ -164,4 +170,99 @@ func syncBackupSecretRefAndCredentialsRef(backup *gardencorev1beta1.Backup) {
 	// - both fields are unset -> we have nothing to sync
 	// - both fields are set -> let the validation check if they are correct
 	// - credentialsRef refer to WorkloadIdentity -> secretRef should stay unset
+}
+
+func cleanupOldPrometheusFolders(ctx context.Context, log logr.Logger, seedClient client.Client) error {
+	clusterList := &extensionsv1alpha1.ClusterList{}
+	if err := seedClient.List(ctx, clusterList); err != nil {
+		return fmt.Errorf("failed to list clusters for cleaning up old Prometheus folders: %w", err)
+	}
+
+	for _, cluster := range clusterList.Items {
+		cleanupPod := &corev1.Pod{
+			ObjectMeta: metav1.ObjectMeta{
+				Namespace: cluster.Name,
+				Name:      "cleanup-obsolete-prometheus-folder",
+			},
+		}
+
+		if err := seedClient.Delete(ctx, cleanupPod); client.IgnoreNotFound(err) != nil {
+			return fmt.Errorf("failed to delete cleanup Pod for Prometheus in cluster %s: %w", cluster.Name, err)
+		}
+
+		prometheus := &monitoringv1.Prometheus{}
+		if err := seedClient.Get(ctx, client.ObjectKey{Namespace: cluster.Name, Name: "shoot"}, prometheus); err != nil {
+			if apierrors.IsNotFound(err) {
+				return nil
+			}
+			return fmt.Errorf("failed to get Prometheus for cluster %s: %w", cluster.Name, err)
+		}
+
+		needsMigration := true
+		for _, initContainer := range prometheus.Spec.InitContainers {
+			if initContainer.Name == "cleanup-obsolete-folder" {
+				needsMigration = false
+				break
+			}
+		}
+
+		if needsMigration {
+			cleanupContainer := corev1.Container{
+				Name:            "cleanup-obsolete-folder",
+				Image:           *prometheus.Spec.Image,
+				ImagePullPolicy: corev1.PullIfNotPresent,
+				Command:         []string{"rm", "-rf", "/prometheus/prometheus-"},
+				VolumeMounts: []corev1.VolumeMount{{
+					Name:      "prometheus-db",
+					MountPath: "/prometheus",
+				}},
+			}
+
+			if err := seedClient.Get(ctx, client.ObjectKey{Namespace: cluster.Name, Name: "prometheus-shoot"}, &corev1.Pod{}); client.IgnoreNotFound(err) != nil {
+				return fmt.Errorf("failed to get Pod for Prometheus in cluster %s: %w", cluster.Name, err)
+			} else if prometheus.Spec.Replicas != nil && *prometheus.Spec.Replicas == 0 && err != nil {
+				cleanupPod.Spec = corev1.PodSpec{
+					RestartPolicy: corev1.RestartPolicyNever,
+					Containers:    []corev1.Container{cleanupContainer},
+					Volumes: []corev1.Volume{{
+						Name: "prometheus-db",
+						VolumeSource: corev1.VolumeSource{
+							PersistentVolumeClaim: &corev1.PersistentVolumeClaimVolumeSource{
+								ClaimName: "prometheus-db-prometheus-shoot-0",
+							},
+						},
+					}},
+				}
+
+				if err := seedClient.Create(ctx, cleanupPod); client.IgnoreAlreadyExists(err) != nil {
+					return fmt.Errorf("failed to create cleanup Pod for Prometheus in cluster %s: %w", cluster.Name, err)
+				}
+
+				return retry.Until(ctx, 2*time.Second, func(ctx context.Context) (done bool, err error) {
+					pod := &corev1.Pod{}
+					if err = seedClient.Get(ctx, client.ObjectKeyFromObject(cleanupPod), pod); err != nil {
+						return retry.MinorError(fmt.Errorf("Error getting Pod: %v", err))
+					}
+
+					if pod.Status.Phase != corev1.PodSucceeded {
+						return retry.MinorError(fmt.Errorf("Pod Status is not succeeded: %v", pod.Status.Phase))
+					}
+
+					return retry.Ok()
+				})
+			}
+
+			prometheus.Spec.InitContainers = append(prometheus.Spec.InitContainers, cleanupContainer)
+			patch := client.MergeFrom(prometheus.DeepCopy())
+			if err := seedClient.Patch(ctx, prometheus, patch); err != nil {
+				return fmt.Errorf("failed to patch Prometheus for cluster %s: %w", cluster.Name, err)
+			}
+		}
+
+		if err := seedClient.Delete(ctx, cleanupPod); client.IgnoreNotFound(err) != nil {
+			return fmt.Errorf("failed to delete cleanup Pod for Prometheus in cluster %s: %w", cluster.Name, err)
+		}
+	}
+
+	return nil
 }
