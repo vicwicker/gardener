6160a0dc5426e287b574525ea18e1a0a5e26e373
Victor Herrero Otal <victor.herrero.otal@sap.com>
2025-08-28 13:06:43 +0200

    idea: Expose HealthChecker as dependency of Health
    This decouples the relationship between a Health object and a
    HealthChecker object for seed health checks. This favors test writing as
    test-specific HealthChecker objects can be used to test the Health
    objects.
    
    Co-authored-by: Istvan Zoltan Ballok <istvan.zoltan.ballok@sap.com>

--- pkg/gardenlet/controller/seed/care/reconciler.go
+++ pkg/gardenlet/controller/seed/care/reconciler.go
@@ -23,6 +23,7 @@ import (
 	"github.com/gardener/gardener/pkg/controllerutils"
 	gardenletconfigv1alpha1 "github.com/gardener/gardener/pkg/gardenlet/apis/config/v1alpha1"
 	kubernetesutils "github.com/gardener/gardener/pkg/utils/kubernetes"
+	"github.com/gardener/gardener/pkg/utils/kubernetes/health/checker"
 )
 
 // NewHealthCheck is used to create a new Health check instance.
@@ -70,7 +71,10 @@ func (r *Reconciler) Reconcile(reconcileCtx context.Context, req reconcile.Reque
 		r.SeedClient,
 		r.Clock,
 		r.Namespace,
-		r.conditionThresholdsToProgressingMapping(),
+		checker.NewHealthChecker(
+			r.SeedClient,
+			r.Clock,
+			checker.WithConditionThresholds(r.conditionThresholdsToProgressingMapping())),
 	).Check(
 		ctx,
 		seedConditions,
